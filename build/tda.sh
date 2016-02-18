7d3138d8e0c186c8dfd3e3dd564c82d0f68a2079
marijan milicevic <m.milicevic@onehippo.com>
2016-02-18 23:18:45 +0100

    - move build files - release

--- build/tda.sh
+++ build/tda.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # Unix shell script for starting tda. If you have big log files
 # you might need to adjust Xmx setting.
-cd $(dirname $0)/..
-java -Xmx512m -jar ./tda.jar 
+java -Xmx512m -jar tda.jar
